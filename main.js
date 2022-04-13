23c46efdd5f44dee947192c3528c0ca93f3dc363
ella choy <egal21de@gmail.com>
2022-04-14 00:37:27 +0200

    size1

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 console.log('test');
 
 // carousel slider bei Aktuelles seite
+
+// const SHOWING_CLASS = "showing";
+// const firstSlide = document.querySelector(".carousel__slider");
+
+// function slide(){
+
+//   const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+
+//   if (currentSlide) { 
+//     currentSlide.classList.remove(SHOWING_CLASS); 
+//     const nextSlide = currentSlide.nextElementSibling; 
+
+//     if (nextSlide) { 
+//       nextSlide.classList.add(SHOWING_CLASS); 
+//     } 
+//     else { 
+//       firstSlide.classList.add(SHOWING_CLASS); 
+//     }
+
+//   } else {
+//     firstSlide.classList.add(SHOWING_CLASS);
+//   }
+// }
+
+// slide();
+// setInterval(slide, 1000);
